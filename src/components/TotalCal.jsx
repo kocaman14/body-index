af3505e96181f58f28f2a3f0c81f50325b1bc771
kocaman14 <163642815+kocaman14@users.noreply.github.com>
2024-10-18 22:54:31 +0300

    Update TotalCal.jsx

--- src/components/TotalCal.jsx
+++ src/components/TotalCal.jsx
@@ -6,6 +6,13 @@ import { heightCal, totalCal, weightCal } from '../features/calculator'
 const TotalCal = () => {
 const{bmi,height,weight}=useSelector((state)=>state.index)
 
+ let statüsColor= bmi > 0 && bmi < 18.5 ? { status: "Low weight", color: "blue" } :
+  bmi >= 18.5 && bmi < 24.9 ? { status: "Normal weight", color: "green" } :
+  bmi >= 25 && bmi < 29.9 ? { status: "Excess weight", color: "pink" } :
+  bmi >= 30 && bmi < 34.9 ? { status: "1st Degree Obese", color: "orange" } :
+  bmi >= 35 && bmi < 39.9 ? { status: "2nd Degree Obese", color: "dark red" } :
+  bmi >= 40 ? { status: "Go to an emergency medical facility", color: "red" } :
+  { status: null, color: null };
 
 console.log(bmi)
   return (
@@ -16,8 +23,12 @@ console.log(bmi)
     <h4>
         {height === 0 && weight === 0
           ? `Please enter height and weight.`
-          : `BMI: ${bmi.toFixed(1)}`}
+          : `BMI: ${bmi.toFixed(5)}`}
       </h4>
+          <span style={{color:statüsColor.color}}>
+            İnfo:{statüsColor.status}
+      
+            </span>        
           </div>
     
   )
